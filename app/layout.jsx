287a81559fa3284ba7ff4837bf33ae2d7128e8e3
Muhammad Tahir <mtahir.kcsf19@iba-suk.edu.pk>
2023-10-14 04:48:48 -0700

    Create a provider

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,25 +4,25 @@ import Nav from "@components/Nav";
 import Provider from "@components/Provider";
 
 export const metadata = {
-    title: "Promptly",
-    description: "Discover & Share AI Prompts",
-  };
+  title: "Promptly",
+  description: "Discover & Share AI Prompts",
+};
 
-  const RootLayout = ({ children }) => (
-    <html lang='en'>
-      <body>
-        {/* <Provider> */}
-          <div className='main'>
-            <div className='gradient' />
-          </div>
-  
-          <main className='app'>
-            <Nav />
-            {children}
-          </main>
-        {/* </Provider> */}
-      </body>
-    </html>
-  );
-  
-  export default RootLayout;
+const RootLayout = ({ children }) => (
+  <html lang="en">
+    <body>
+      <Provider>
+        <div className="main">
+          <div className="gradient" />
+        </div>
+
+        <main className="app">
+          <Nav />
+          {children}
+        </main>
+      </Provider>
+    </body>
+  </html>
+);
+
+export default RootLayout;
--- components/Provider.jsx
+++ components/Provider.jsx
@@ -1,7 +1,12 @@
+'use client';
 
-const Provider = () => {
+import { SessionProvider } from "next-auth/react"
+
+const Provider = ({ children, session }) => {
   return (
-    <div>Provider</div>
+    <SessionProvider session={session}>
+      {children}
+    </SessionProvider>
   )
 }
 
