283aae315a2d1a5f7468e5a81e6e19962587ec07
Muhammad Tahir <mtahir.kcsf19@iba-suk.edu.pk>
2023-10-14 02:19:44 -0700

    Add Data into Home page

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,21 @@
-
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text_center">Discover & Share</h1>
+      <br className="max-md:hidden" />
+      <span className="head_text orange_gradient text-center">
+        {" "}
+        AI - Powered Prompts
+      </span>
 
-export default Home
+      <p className="desc text-center">
+        Promptly is an open-source AI prompting tool for modern world to
+        discover, create and share creative prompts.
+      </p>
+
+      {/* Feed  component*/}
+    </section>
+  );
+};
+
+export default Home;
