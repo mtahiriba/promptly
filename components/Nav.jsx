80f5270a158469c9569c7e88c2aa3173aa67c46d
Muhammad Tahir <mtahir.kcsf19@iba-suk.edu.pk>
2023-10-14 04:16:31 -0700

    Done with Mobile Navigation

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -9,6 +9,7 @@ const Nav = () => {
 
   const isUserLoggedIn = true;
   const [providers, setProviders] = useState(null);
+  const [toggleDropdown, setToggleDropdown] = useState(false)
 
   useEffect(() => {
     const setProviders = async () => {
@@ -40,7 +41,7 @@ const Nav = () => {
               href={"/create-prompt"}
               className="black_btn"  
             >
-              Create Post
+              Create Prompt
             </Link>
 
             <button type="button" onClick={signOut} className="outline_btn">
@@ -75,6 +76,69 @@ const Nav = () => {
           </>
         )}
       </div>
+
+
+      {/* Mobile Navigation */}
+      <div className="sm:hidden flex relative">
+          {isUserLoggedIn ? (
+            <div className="flex">
+              <Image
+                src={'/assets/images/logo.svg'}
+                alt="profile"
+                width={37}
+                height={37}
+                className="rounded-full"
+                onClick={()=> setToggleDropdown((prev) => !prev)}
+              />
+
+              {toggleDropdown && (
+                <div className="dropdown">
+                  <Link 
+                    href={'/profile'}
+                    className="dropdown-link"
+                    onClick={() => setToggleDropdown(false)}
+                  >
+                    My Profile
+                  </Link>
+                  <Link 
+                    href={'/create-prompt'}
+                    className="dropdown-link"
+                    onClick={() => setToggleDropdown(false)}
+                  >
+                    Create Prompt
+                  </Link>
+                  <button
+                    type="button"
+                    onClick={()=>{
+                      setToggleDropdown(false);
+                      signOut();
+                    }}
+                    className="mt-5 w-full black_btn"
+                  >
+                    Sign out
+                  </button>
+                </div>
+              )}
+
+            </div>
+          ) :(
+            <>
+            {providers && (
+              Object.values(providers).map((provider) => (
+                <button 
+                  type="button"
+                  key={provider.name}
+                  onClick={() => signIn(provider.id)}  
+                  className="black_btn"
+                >
+                   Sign in
+                </button>
+              )))
+            }
+          </>
+          )}
+      </div>
+
     </div>
   )
 }
