aeb63c6d1ca493e19f3fcf5088ac6fc85fc87649
Muhammad Tahir <mtahir.kcsf19@iba-suk.edu.pk>
2023-10-15 06:19:16 -0700

    Add search bar and create API for fetchin Prompts from database

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,8 +1,83 @@
+'use client';
+
+import { useState, useEffect } from "react";
+import PromptCard from "./PromptCard";
+
+const PromptCardList = ({ data, handleTagClick }) => {
+  return (
+    <div className='mt-16 prompt_layout'>
+      {data.map((post) => (
+        <PromptCard
+          key={post._id}
+          post={post}
+          handleTagClick={handleTagClick}
+        />
+      ))}
+    </div>
+  );
+};
 
 const Feed = () => {
-  return (
-    <div>Feed</div>
-  )
-}
+  const [allPosts, setAllPosts] = useState([]);
 
-export default Feed
+  // Search states
+  const [searchText, setSearchText] = useState("");
+  const [searchTimeout, setSearchTimeout] = useState(null);
+  const [searchedResults, setSearchedResults] = useState([]);
+
+  const fetchPosts = async () => {
+    const response = await fetch("/api/prompt");
+    const data = await response.json();
+
+    setAllPosts(data);
+  };
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+
+  const handleSearchChange = (e) => {
+    clearTimeout(searchTimeout);
+    setSearchText(e.target.value);
+
+    // debounce method
+    // setSearchTimeout(
+    //   setTimeout(() => {
+    //     const searchResult = filterPrompts(e.target.value);
+    //     setSearchedResults(searchResult);
+    //   }, 500)
+    // );
+  };
+
+  return (
+    <section className="feed">
+      <form className="relative w-full flex-center">
+        <input
+          type="text"
+          placeholder="Search for a tag or a username"
+          value={searchText}
+          onChange={handleSearchChange}
+          required
+          className="search_input peer"
+        />
+      </form>
+
+      {/* All Prompts */}
+      {/* {searchText ? (
+        <PromptCardList
+          data={searchedResults}
+          handleTagClick={handleTagClick}
+        />
+      ) : ( */}
+        <PromptCardList 
+          data={allPosts}
+          handleTagClick={() => { }} 
+          // handleTagClick={handleTagClick} 
+        />
+      {/* )} */}
+    </section>
+  );
+};
+
+export default Feed;
