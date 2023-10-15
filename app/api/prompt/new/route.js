8e2d78cc79c0ca6969b46333fd40ba5843c37ef4
Muhammad Tahir <mtahir.kcsf19@iba-suk.edu.pk>
2023-10-15 05:49:09 -0700

    Create API for new Prompt

---
+++ app/api/prompt/new/route.js
@@ -0,0 +1,14 @@
+import { connectToDB } from "@utils/database";
+
+export const POST = async (req) => {
+
+    const { userId, prompt, tag} = await req.json();
+
+    try {
+        await connectToDB();
+        
+    } catch (error) {
+        
+    }
+
+}
--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -2,7 +2,7 @@
 
 import { useState } from "react";
 import { useSession } from "next-auth/react";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 
 import Form from "@components/Form";
 
@@ -20,24 +20,24 @@ const CreatePrompt = () => {
     e.preventDefault();
     setSubmitting(true);
 
-    // try {
-    //   const response = await fetch("/api/prompt/new", {
-    //     method: "POST",
-    //     body: JSON.stringify({
-    //       prompt: post.prompt,
-    //       userId: session?.user.id,
-    //       tag: post.tag,
-    //     }),
-    //   });
+    try {
+      const response = await fetch("/api/prompt/new", {
+        method: "POST",
+        body: JSON.stringify({
+          prompt: post.prompt,
+          userId: session?.user.id,
+          tag: post.tag,
+        }),
+      });
 
-    //   if (response.ok) {
-    //     router.push("/");
-    //   }
-    // } catch (error) {
-    //   console.log(error);
-    // } finally {
-    //   setSubmitting(false);
-    // }
+      if (response.ok) {
+        router.push("/");
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setSubmitting(false);
+    }
   };
 
   return (
